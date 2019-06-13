792fe1144e1928f2e0ca60be8795bc956605a425
Cijams <cijams@asu.edu>
2019-06-12 20:23:25 -0700

    Update to explorer page.

--- block-explorer/javascript/explore-block.js
+++ block-explorer/javascript/explore-block.js
@@ -1,7 +1,27 @@
 function getBlockData() {
     $(".table tbody").empty();
     let blockHeight = document.getElementById("blockSearch").elements[0].value;
-    $.getJSON(`https://cors.io/?https://blockchain.info/block-height/${blockHeight}?format=json`, function (data) {
+    $.getJSON(`https://api.blockcypher.com/v1/btc/main/blocks/${blockHeight}`, function (data) {
+
+        let blockData = JSON.stringify(data);
+
+        parser = blockData.split(",");
+        parser = cleanParser(parser);
+
+
+        parser.forEach(function (element) {
+            let table = element.split(":");
+            let field = table[0].replace(/[&\/\\#,+()$~%.'":*?<>{}[\]]/g, '');
+            let data = table[1].replace(/[&\/\\#,+()$~%.'":*?<>{}[\]]/g, '');
+            let markup = "<tr><td>" + field + "</td><td>" + data;
+            $("table tbody").append(markup);
+        });
+
+
+
+
+        // `https://api.blockcypher.com/v1/btc/main/blocks/${blockHeight}?txstart=1&limit=1`
+        /*
         let blockData = JSON.stringify(data);
         blockData = blockData.substr(12, blockData.length);
         let parser = blockData.split(",");
@@ -14,11 +34,14 @@ function getBlockData() {
             let markup = "<tr><td>" + field + "</td><td>" + data;
             $("table tbody").append(markup);
         });
+        */
     });
+
 }
 
 function cleanParser(parser) {
-    let data = [13, 0, 2, 3, 5, 6, 7, 8, 10, 11, 4];
+    let data = [0, 1, 3, 4, 5, 6, 7, 10,
+    11,12, 13 , 14, 15, 16, 17];
     let blockData = [];
     for(let i = 0; i < data.length; i++) {
         blockData.push(parser[data[i]]);
